0df343c3ce3efa9960e119381c7cba7bc6d22268
Pranab Amatya <pranab.aamtya.pa@gmail.com>
2021-08-29 09:38:50 +1000

    Minor fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5b19b2445c919faa18b6"; }
+/******/ 		__webpack_require__.h = function() { return "70dd97f2cf0b87493c4b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,39 +1,58 @@
-import React from 'react';
+import React from "react";
 
-import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { projects } from '../../constants/constants';
+import {
+  BlogCard,
+  CardInfo,
+  ExternalLinks,
+  GridContainer,
+  HeaderThree,
+  Hr,
+  Tag,
+  TagList,
+  TitleContent,
+  UtilityList,
+  Img,
+} from "./ProjectsStyles";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { projects } from "../../constants/constants";
 
 const Projects = () => (
   <Section id='projects'>
     <SectionDivider />
-    <SectionTitle main >Projects</SectionTitle>
-    <SectionText>Here are few of the projects I have been involved with.</SectionText>
+    <SectionTitle main>Projects</SectionTitle>
+    <SectionText>
+      Here are few of the projects I have been involved with.
+    </SectionText>
     <GridContainer>
-      {projects.map(({ id, title, image, description, tags, source, visit }) => (
-        <BlogCard key={id}>
-          <Img src={image} />
-          <TitleContent>
-            <HeaderThree title>
-              {title}
-            </HeaderThree>
-            <Hr />
-            <CardInfo>{description}</CardInfo>
-            <div>
-              <TitleContent>Stack</TitleContent>
-              <TagList>
-                {tags.map((tag, i) => (
-                  <Tag key={i}>{tag}</Tag>
-                ))}
-              </TagList>
-            </div>
-          </TitleContent>
-          <UtilityList>
-            <ExternalLinks href={visit}>Code</ExternalLinks>
-            <ExternalLinks href={source}>Source</ExternalLinks>
-          </UtilityList>
-        </BlogCard>
-      ))}
+      {projects.map(
+        ({ id, title, image, description, tags, source, visit }) => (
+          <BlogCard key={id}>
+            <Img src={image} />
+            <TitleContent>
+              <HeaderThree title>{title}</HeaderThree>
+              <Hr />
+              <CardInfo>{description}</CardInfo>
+              <div>
+                <TitleContent>Stack</TitleContent>
+                <TagList>
+                  {tags.map((tag, i) => (
+                    <Tag key={i}>{tag}</Tag>
+                  ))}
+                </TagList>
+              </div>
+            </TitleContent>
+            <UtilityList>
+              <ExternalLinks href={visit}>Visit</ExternalLinks>
+              <ExternalLinks href={source}>Source</ExternalLinks>
+            </UtilityList>
+          </BlogCard>
+        )
+      )}
     </GridContainer>
   </Section>
 );
